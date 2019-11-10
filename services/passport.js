cccc18d1a2a0714e9dcebf8af8d3b8ad8a0b43e7
ezywebs <ec2-user@ip-172-31-22-240.us-east-2.compute.internal>
2019-11-10 20:39:19 +0000

    env setup

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,32 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then((user) => {
+      done(null, user);  
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'http://fd951ce0879a41fbbb003d8894cc0952.vfs.cloud9.us-east-2.amazonaws.com/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        new User({ googleId: profile.id }).save();
-        
+        User.findOne({ googleId: profile.id })
+         .then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id })
+                .save()
+                .then(user => done(null, user));
+            }
+         })
     })
 );
