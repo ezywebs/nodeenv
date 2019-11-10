62ea0091c9e1e58ce6954e7ce466b133c8788986
EC2 Default User <ec2-user@ip-172-31-22-240.us-east-2.compute.internal>
2019-11-10 01:58:11 +0000

    change double quotes to singles

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
-const passport = require("passport");
-const express = require("express");
+const passport = require('passport');
+const express = require('express');
 
 module.exports = (app) => {
     app.get(
-        "/auth/google", 
-        passport.authenticate("google", {
-            scope: ["profile", "email"]
+        '/auth/google', 
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
         })
     );
     
-    app.get("/auth/google/callback", passport.authenticate("google"));
+    app.get('/auth/google/callback', passport.authenticate('google'));
 }
 
